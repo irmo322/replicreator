559f010a818d3db312e7c19a5d6394b829a283b6
Benjamin Maréchal <irmo322@gmail.com>
2022-10-18 18:53:50 +0200

    Adding a check for parameters and main functions to implement later.

--- src/replicator/app.py
+++ src/replicator/app.py
@@ -36,6 +36,43 @@ def process_parameters_file(parameters_file_path):
 
 
 def process_parameters(parameters):
-    print(parameters)
+    print(parameters)  # TODO erase it
+
+    check_parameters(parameters)
+    check_transcriptions(parameters)
+    generate_statistics(parameters)
+    generate_web_app(parameters)
 
 
+def check_parameters(parameters):
+    """
+    Checks if parameters are consistent and raises exceptions if not.
+    :param parameters:
+    :return:
+    """
+    # Checking that labels from characters and stage directions do not conflict with each other.
+    char2labels = {}
+    char2labels["stage directions"] = parameters["stage_directions"]["labels"]
+    for i, character in enumerate(parameters["characters"]):
+        char2labels[f"character {i}"] = character["labels"]
+    label2char = {}
+    for char, char_labels in char2labels.items():
+        for label in char_labels:
+            if label in label2char:
+                raise RuntimeError(f"label {label} is present twice in {label2char[label]} and {char}.")
+            label2char[label] = char
+
+
+def check_transcriptions(parameters):
+    # 2 - vérifier la cohérence des transcriptions, notamment vis-à-vis des paramètres
+    pass  # TODO
+
+
+def generate_statistics(parameters):
+    # 3 - générer les statistiques
+    pass  # TODO
+
+
+def generate_web_app(parameters):
+    # 4 - générer la web app
+    pass  # TODO
