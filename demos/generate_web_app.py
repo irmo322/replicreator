48924047f73225788564cef83dc1398bdc1d23c0
Benjamin Maréchal <irmo322@gmail.com>
2022-10-19 22:07:30 +0200

    Renaming project to replicreator.

--- demos/generate_web_app.py
+++ demos/generate_web_app.py
@@ -1,8 +1,8 @@
-from replicator.app import process_parameters_file
+from replicreator.app import process_parameters_file
 
 
 def main():
-    process_parameters_file("replicator_parameters.yaml")
+    process_parameters_file("replicreator_parameters.yaml")
 
 
 if __name__ == '__main__':
